d7691f5a93a475e07095e8a9955622507fc833b3
Grubinio <georggruben@gmail.com>
2023-11-30 19:10:17 +0100

    Komplett umgeschrieben. Jetzt ist die Seite ein Spiel.

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-var _ = require('lodash');
-console.log(_);
+const options = ["Stein", "Schere", "Papier"]
+let oponentSelection = options[Math.floor(Math.random() * 3)];
+let playerSelection = document.getElementById("wurf").value;
+console.log(playerSelection);
 
-let array = [1,2,3,4,5];
-console.log('answer:', _.without(array, 3));
+document.getElementById("myBtton").addEventListener("click", myFunction);
+
+function myFunction() {
+    playerSelection = document.getElementById("wurf").value;
+ document.getElementById("output").innerHTML = spiel(playerSelection, oponentSelection);
+  }
+
+
+
+
+function spiel(playerSelection, oponentSelection){
+    if (playerSelection === oponentSelection){
+        return "Ihr habt beide " + playerSelection + " gewählt. Noch mal!";
+    }
+    else{
+        return "Deine Auswahl: " + playerSelection + ". Dein Gegner: " + oponentSelection +  ". " + spielEntscheidung(playerSelection, oponentSelection);
+
+    }
+}
+
+function spielEntscheidung(playerSelection, oponentSelection){
+  
+    if (playerSelection === "Stein"){
+        if (oponentSelection === "Schere"){
+            return "Du gewinnst!"
+        }
+        if (oponentSelection === "Papier"){
+            return "Du verlierst!"
+        }
+    }
+    if (playerSelection === "Schere"){
+        if (oponentSelection === "Papier"){
+            return "Du gewinnst!"
+        }
+        if (oponentSelection === "Stein"){
+            return "Du verlierst!"
+        }
+    }
+    if (playerSelection === "Papier"){
+        if (oponentSelection === "Stein"){
+            return "Du gewinnst!"
+        }
+        if (oponentSelection === "Schere"){
+            return "Du verlierst!"
+        }
+    }
+}
+
